53c73be60bc092bd04f09e2f405a375632475db4
Konstantin Mahortov <cs@xcsm.ru>
2018-01-29 22:29:47 +0300

    Add project files.

---
+++ DataMos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataMos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
