1ea3ce91a5ec3953030e8ceb966ae05f565e15c3
Konstantin Mahortov <cs@xcsm.ru>
2018-01-29 22:38:25 +0300

    Base

---
+++ Logger/Logger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    public class Logger
+    {
+        String dirLog;
+        String fileLogName;
+        String fileLogExtension = ".log";
+        public Logger()
+        {
+            fileLogName = String.Format("{0}{1}", DateTime.Today, fileLogExtension);
+        }
+        public static void Warn(String wagingText)
+        {
+
+        }
+
+        public static void Error(String ErrorTxt)
+        {
+            
+        }
+
+        public static void Info(String InfoTxt)
+        {
+
+        }
+    }
+}
